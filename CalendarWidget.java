77829425bcd1425a7e5de1a9841d19d03c41bb46
Anton Koch <antonkoch@Mac.fritz.box>
2025-06-29 18:35:39 +0200

    adding CalendarPanel

---
+++ CalendarWidget.java
@@ -0,0 +1,15 @@
+
+/**
+ * Beschreiben Sie hier die Klasse CalendarWidget.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+public class CalendarWidget extends JPanel
+{
+     
+}
